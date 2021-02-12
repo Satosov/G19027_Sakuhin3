9c0d58aaa685b838a9b1ab7ad786812834a5669f
Satosov <hj-g19027@sist.ac.jp>
2021-02-12 16:27:49 +0900

    マシンガンの追加 敵の情報を仮で追加

--- main.cpp
+++ main.cpp
@@ -9,15 +9,15 @@
 #define WINDOW_BAR_ON	0
 #define GAME_TITLE		"直進する巨大兵器"
 
-#define MAP_KIND_MAX	3
+//#define MAP_KIND_MAX	3
 
 #define SG_MAX		5		//散弾は最大5発 要リロード
+#define MG_MAX	4		//機関銃は最大500発 要リロード
 //#define ROCKET_MAX		1		//最大1発
-//#define MG_MAX	500		//機関銃は最大500発 要リロード
 //#define EG_MAX	1		//最大1発
-#define TAMA_MAX	255
+//#define TAMA_MAX	255
 
-#define ENEMY_HELI_WEAPON_MAX	3
+#define ENEMY_MAX	100
 
 VOID MY_START(VOID);
 VOID MY_START_DRAW(VOID);
@@ -49,6 +49,8 @@ typedef struct MY_IMAGE
 	int Handle;		//画像の管理番号
 	int x;
 	int y;
+	int x2;			//四角形
+	int y2;
 	int width;		//画像の幅
 	int height;		//画像の高さ
 	double radian;	//武器の角度
@@ -62,25 +64,25 @@ typedef struct STRUCT_TAMA
 	int x;
 	int y;
 	BOOL IsView;
-	int speed;
+	double radian;		//弾の角度
 }TAMA;
 
 typedef struct CHARA_INFO		//キャラクターのデータ
 {
 	IMAGE image;
 	int hp;
-	int speed;
 
-	BOOL Canshot;
+	BOOL SGCanshot;			//SGが打てるがどうか
+	BOOL MGCanshot;
 	int ShotReloadCnt;
 	int ShotReloadCntMAX;
 
-	TAMA tama[SG_MAX];
-
-	RECT coll;
+	TAMA sgtama[SG_MAX];
+	TAMA mgtama[MG_MAX];
 }CHARA;
 
 CHARA player;		//プレイヤー
+CHARA enemy[ENEMY_MAX];		//敵
 
 //MY_IMAGE image_background;
 //MY_IMAGE image_player;			//自機
@@ -113,7 +115,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	//image_player_weapon.x = 160;
 	//image_player_weapon.y = 160;
 	player.ShotReloadCnt = 0;
-	player.ShotReloadCntMAX = 30;	//今のところあまり意味なし
+	player.ShotReloadCntMAX = 60;	//リロード
 
 
 	while (TRUE)
@@ -213,15 +215,25 @@ BOOL MY_KEYDOWN_KEEP(int KEY_INPUT_, int DownTime)
 
 VOID MY_START(VOID)
 {
-
 	if (AllKeyState[KEY_INPUT_RETURN] == 1)
 	{
 		gamescene = SCENEPLAYING;
 	}
 
+	//弾の初期化
 	for (int cnt = 0; cnt < SG_MAX; cnt++)
 	{
-		player.tama[cnt].IsView = FALSE;
+		player.sgtama[cnt].IsView = FALSE;
+	}
+	for (int cnt = 0; cnt < MG_MAX; cnt++)
+	{
+		player.mgtama[cnt].IsView = FALSE;
+	}
+
+	for (int cnt = 0; cnt < ENEMY_MAX; cnt++)
+	{
+		enemy[cnt].image.IsView = FALSE;
+		//enemy[cnt].hp = 0;			//hpの初期化
 	}
 
 	return;
@@ -238,46 +250,82 @@ VOID MY_START_DRAW(VOID)
 
 VOID MY_PLAY(VOID)
 {
+	for (int cnt = 0; cnt < ENEMY_MAX; cnt++)		//プレイが始まると同時に描画開始
+	{
+		enemy[cnt].image.IsView = TRUE;
+		break;
+	}
 	player.image.x = 100;		//仮
 	player.image.y = 150;
+	player.image.x2 = 200;
+	player.image.y2 = 499;
 
+	enemy[0].image.x = 495;
+	enemy[0].image.x2 = 505;
+	enemy[0].image.y = 165;
+	enemy[0].image.y2 = 175;
 
-	if (AllKeyState[KEY_INPUT_SPACE] == 1)
+	if (AllKeyState[KEY_INPUT_SPACE] == TRUE)
 	{
 		gamescene = SCENEEND;
 	}
-	if (AllKeyState[KEY_INPUT_1] == 1)
+
+	if (AllKeyState[KEY_INPUT_Z] == 1)			//Zを押したときSG
 	{
-		if (player.Canshot == TRUE)		//撃てるとき
+		if (player.SGCanshot == TRUE)		//撃てるとき
 		{
-			player.Canshot = FALSE;
+			player.SGCanshot = FALSE;
 			
 			for (int cnt = 0; cnt < SG_MAX; cnt++)
 			{
-				if (player.tama[cnt].IsView == FALSE)
+				if (player.sgtama[cnt].IsView == FALSE)
 				{
-					player.tama[cnt].x = 170;		//仮座標
-					player.tama[cnt].y = 170;
-					player.tama[cnt].IsView = TRUE;
+					player.sgtama[cnt].x = 170;		//仮座標
+					player.sgtama[cnt].y = 170;
+					player.sgtama[cnt].IsView = TRUE;
 					//break;
 				}
 			}
 		}
 	}
 
-	//弾が打てないとき
-	if (player.Canshot == FALSE)
+	//弾が打てないとき	SG
+	if (player.SGCanshot == FALSE)
 	{
 		if (player.ShotReloadCnt == player.ShotReloadCntMAX)
 		{
 			player.ShotReloadCnt = 0;
-			player.Canshot = TRUE;		//リロード完了
+			player.SGCanshot = TRUE;		//リロード完了
 		}
 
 		player.ShotReloadCnt++;
 	}
 
-	
+	if (AllKeyState[KEY_INPUT_X] == TRUE)		//マシンガンの弾の生成
+	{
+		if (player.MGCanshot == TRUE)			//撃てるとき
+		{
+			for (int cnt = 0; cnt < MG_MAX; cnt++)
+			{
+				player.mgtama[cnt].x = 170;		//仮座標
+				player.mgtama[cnt].y = 210;
+				player.mgtama[cnt].IsView = TRUE;
+				break;
+			}
+		}
+	}
+
+	//弾が打てないとき　MG
+	if (player.MGCanshot == FALSE)
+	{
+		if (player.ShotReloadCnt == player.ShotReloadCntMAX)
+		{
+			player.ShotReloadCnt = 0;
+			player.MGCanshot = TRUE;
+		}
+
+		player.ShotReloadCnt++;
+	}
 
 	return;
 }
@@ -286,32 +334,65 @@ VOID MY_PLAY_DRAW(VOID)
 {
 	DrawString(0, 0, "play\npush :SPACE", RGB(255, 255, 255));
 
+	DrawBox(player.image.x, player.image.y, player.image.x2,player.image.y2, RGB(255, 0, 0), TRUE);	//自機の当たり判定
 
-	DrawBox(player.image.x, player.image.y, 200, 499, RGB(255, 0, 0), TRUE);	//自機の当たり判定
-
+	//**************************ショットガン********************
 	for (int cnt = 0; cnt < SG_MAX; cnt++)
 	{
-		if (player.tama[cnt].IsView == TRUE)
+		if (player.sgtama[cnt].IsView == TRUE)
 		{
 			DrawCircle(
-				player.tama[cnt].x,
-				player.tama[cnt].y,
+				player.sgtama[cnt].x,
+				player.sgtama[cnt].y,
 				5, RGB(0, 255, 255), TRUE);
 		}
-		if (player.tama[2].x > 1000
-			|| player.tama[2].y > 500
-			|| player.tama[2].y < 0)
+		if (player.sgtama[2].x > WINDOW_WIDTH
+			|| player.sgtama[2].y > WINDOW_HEIGHT
+			|| player.sgtama[2].y < 0)
 		{
-			player.tama[cnt].IsView = FALSE;	//画面外にでると終了
+			player.sgtama[cnt].IsView = FALSE;	//画面外にでると終了
 		}
 		else
 		{
-			player.tama[0].x += 4; player.tama[0].y -= 2;
-			player.tama[1].x += 4; player.tama[1].y -= 1;
-			player.tama[2].x += 4;
-			player.tama[3].x += 4; player.tama[3].y += 1;
-			player.tama[4].x += 4; player.tama[4].y += 2;
+			player.sgtama[0].x += 3; player.sgtama[0].y -= 2;
+			player.sgtama[1].x += 3; player.sgtama[1].y -= 1;
+			player.sgtama[2].x += 3;
+			player.sgtama[3].x += 3; player.sgtama[3].y += 1;
+			player.sgtama[4].x += 3; player.sgtama[4].y += 2;
 		}
+	}
+
+	//**************************マシンガン********************
+	for (int cnt = 0; cnt < MG_MAX; cnt++)
+	{
+		if (player.mgtama[cnt].IsView == TRUE)//描画できるとき
+		{
+			DrawCircle(
+				player.mgtama[cnt].x,
+				player.mgtama[cnt].y,
+				4, RGB(255, 255, 0), TRUE);
+		}
+		if (player.mgtama[cnt].x > WINDOW_WIDTH
+			|| player.mgtama[cnt].y > WINDOW_HEIGHT
+			|| player.mgtama[cnt].y < 0)		//画面外にでると描画終了
+		{
+			player.mgtama[cnt].IsView = FALSE;
+		}
+		else
+		{
+			player.mgtama[cnt].x += 8;
+		}
+	}
+
+	//敵の描画開始
+	if (enemy[0].image.IsView == TRUE)
+	{
+		DrawBox(enemy[0].image.x, enemy[0].image.y, enemy[0].image.x2, enemy[0].image.y2, RGB(0, 0, 255), TRUE);	//敵機仮
+
+			//例　image_tama.x < image_husen[i].x + image_husen[i].width&&
+			//			image_tama.y<image_husen[i].y + image_husen[i].height &&
+			//			image_tama.x + image_tama.width>image_husen[i].x &&
+			//			image_tama.y + image_tama.height>image_husen[i].y
 	}
 
 	return;
